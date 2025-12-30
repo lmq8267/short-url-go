4fe2ab0acd3abb14cd4892c28500baef6cd6aab4
lmq8267 <119713693+lmq8267@users.noreply.github.com>
2025-12-30 21:43:52 +0800

    设置不同平台的帮助信息输出和IP离线数据库目录

--- main.go
+++ main.go
@@ -3665,7 +3665,7 @@ func main() {
 
 	var (
 		port        int
-		showHelp    bool
+		    bool
 		showVersion bool
 		daemon      bool
 		email       string
@@ -3745,27 +3745,7 @@ func main() {
 
 	//打印帮助信息
 	if showHelp {
-		colorText := func(color int, message string) string {
-			return fmt.Sprintf("\x1b[1;%dm%s\x1b[0m", color, message)
-		}
-
-		// 使用固定宽度确保对齐
-		fmt.Printf("  %-20s %-15s %s\n", colorText(36, "-p"), colorText(34, "[端口号]"), "监听指定端口号")
-		fmt.Printf("  %-20s %-15s %s\n", colorText(36, "-d"), colorText(34, "[文件路径]"), "指定本地数据存放的目录路径，默认当前程序路径的./short_data文件夹")
-		fmt.Printf("  %-20s %-15s %s\n", colorText(36, "-db"), colorText(34, "[文件路径]"), "指定IP地址库离线数据存放的目录路径，默认/tmp文件夹")
-		fmt.Printf("  %-20s %-15s %s\n", colorText(36, "-log"), colorText(34, "[文件路径]"), "启用日志，并指定日志存放的目录路径")
-		fmt.Printf("  %-20s %-15s %s\n", colorText(36, "-admin"), "", "启用管理页面管理短链数据，网页路径/admin")
-		fmt.Printf("  %-20s %-15s %s\n", colorText(36, "-e"), colorText(34, "[邮箱地址]"), "指定邮箱地址，修改页面的联系邮箱地址")
-		fmt.Printf("  %-20s %-15s %s\n", colorText(36, "-u"), colorText(34, "[账户名]"), "指定管理页面的登陆账户名")
-		fmt.Printf("  %-20s %-15s %s\n", colorText(36, "-w"), colorText(34, "[密码]"), "指定管理页面的登陆密码")
-		fmt.Printf("  %-20s %-15s %s\n", colorText(36, "-daemon"), "", "以后台模式运行")
-		fmt.Printf("  %-20s %-15s %s\n", colorText(36, "-redis-addr"), colorText(34, "[地址:端口]"), "Redis服务器地址 (例如: localhost:6379)")
-		fmt.Printf("  %-20s %-15s %s\n", colorText(36, "-redis-user"), colorText(34, "[用户名]"), "Redis用户名 (可选)")
-		fmt.Printf("  %-20s %-15s %s\n", colorText(36, "-redis-pass"), colorText(34, "[密码]"), "Redis密码 (可选)")
-		fmt.Printf("  %-20s %-15s %s\n", colorText(36, "-redis-pre"), colorText(34, "[前缀]"), "Redis数据前缀，默认为short，连接相同的redis数据库时用于区分不同应用")
-		fmt.Printf("  %-20s %-15s %s\n", colorText(36, "-v"), "", "版本号")
-		fmt.Printf("  %-20s %-15s %s\n", colorText(36, "-h"), "", "帮助信息")
-
+		printHelp()
 		return
 	}
 
@@ -3800,7 +3780,7 @@ func main() {
 
 	// 获取IP离线数据存放目录
 	if dbDir == "" {
-		dbDir = "/tmp" // 默认路径为 /tmp
+		dbDir = getDefaultDBDir()
 	}
 	if _, err := os.Stat(dbDir); os.IsNotExist(err) {
 		err := os.MkdirAll(dbDir, 0755)
